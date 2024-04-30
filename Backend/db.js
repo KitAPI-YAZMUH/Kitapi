55165ef7c076f0cb7ba98dea69e785d7d0a98f5c
Serdar AYVALI <serdar.ayvali@hotmail.com>
2024-04-30 13:27:52 +0300

    Hesap bilgileri PostreSql de tutulmaya başladı. Tüm sayfalar EJS uyumlu  hale getirildi ve ortak bir üstbilgi ve altbilgiyle standartlaştırıldı.

--- Backend/db.js
+++ Backend/db.js
@@ -1,10 +1,51 @@
-const Pool = require("pg").Pool;
+const { Pool } = require('pg');
+require('dotenv').config(); // .env dosyasındaki ortam değişkenlerini yüklemek için dotenv modülünü kullanın
+
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "studentDb", // kendi db nize göre ayarlayın
-    password: "",
-    port: "5432"
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 });
+
+// Tablo var mı kontrol et ve yoksa oluştur
+pool.query(
+    `SELECT EXISTS (
+        SELECT 1
+        FROM   information_schema.tables 
+        WHERE  table_schema = 'public'
+        AND    table_name = 'users'
+    );`,
+    (error, results) => {
+        if (error) {
+            console.error('Tablo varlığı kontrol edilirken bir hata oluştu:', error);
+            return;
+        }
+        
+        const tableExists = results.rows[0].exists;
+        if (!tableExists) {
+            // Tablo yoksa oluştur
+            pool.query(
+                `CREATE TABLE users (
+                    id SERIAL PRIMARY KEY,
+                    username VARCHAR(100),
+                    surname VARCHAR(100),
+                    email VARCHAR(100) UNIQUE,
+                    password VARCHAR(255)
+                );`,
+                (error, results) => {
+                    if (error) {
+                        console.error('Tablo oluşturulurken bir hata oluştu:', error);
+                        return;
+                    }
+                    console.log('Users tablosu oluşturuldu');
+                }
+            );
+        } else {
+            console.log('Users tablosu zaten mevcut');
+        }
+    }
+);
 
 module.exports = pool;
--- Backend/server.js
+++ Backend/server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const bookRouters = require('./src/books/routes.js'); // Kitap rotaları
 const bodyParser = require('body-parser'); // Gelen isteklerdeki verileri okumak için body-parser kullanılır
 const crypto = require('crypto-js'); // Şifreleme işlemleri için crypto-js kullanılır
-const fs = require('fs'); // Dosya işlemleri için fs modülü kullanılır
 const path = require('path'); // Dosya yollarını düzenlemek için path modülü kullanılır
 const request = require('request');
+const db = require('./db'); // Veritabanı bağlantısı için db modülünü ekledik
+const pool = db;
 
 const app = express();
 const PORT = process.env.PORT || 5000; // Sunucu portu belirlenir, varsayılan olarak 3000 kullanılır
@@ -32,11 +33,9 @@ app.use((req, res, next) => {
 });
 
 
-
 // Ana sayfa
 app.get('/', (req, res) => {
     res.render('index'); // index.ejs dosyasını render et
-    //res.sendFile(path.join(__dirname, frontendDirectory, 'index.html')); // Ana sayfa dosyası gönderilir
 });
 
 app.get('/index', (req, res) => {
@@ -51,20 +50,29 @@ app.get('/cart', (req, res) => {
     res.render('cart'); // cart.ejs dosyasını render et
 });
 
-
 app.get('/prompt', (req, res) => {
     res.render('prompt'); // prompt.ejs dosyasını render et
 });
-
 
 app.get('/contact', (req, res) => {
     res.render('contact'); // contact.ejs dosyasını render et
 });
 
+app.get('/chackout', (req, res) => {
+    res.render('chackout'); // chackout.ejs dosyasını render et
+});
+
+app.get('/testimonial', (req, res) => {
+    res.render('testimonial'); // testimonial.ejs dosyasını render et
+});
+
+app.get('/404', (req, res) => {
+    res.render('404'); // 404.ejs dosyasını render et
+});
 
 // Kayıt olma sayfası   
 app.get('/signup', (req, res) => {
-    res.sendFile(path.join(frontendDirectory, 'signup.html')); // Kayıt olma sayfası dosyası gönderilir
+    res.render('signup');
 });
 
 // Kayıt olma işlemi
@@ -73,72 +81,63 @@ app.post('/signup', (req, res) => {
 
     // Kullanıcı şifresi hashlenir
     const hashedPassword = crypto.SHA256(password).toString();
-    // Yeni kullanıcı objesi oluşturulur
-    const newUser = { username, surname, email, password: hashedPassword };
 
-    // Kullanıcılar dosyası okunur
-    fs.readFile('users.txt', 'utf8', (err, data) => {
-        if (err) throw err;
-        let users = JSON.parse(data);
-
-        // Kullanıcı e-postası zaten kullanılmış mı kontrol edilir
-        const existingUser = users.find(u => u.email === email);
-        if (existingUser) {
-            return res.send('Bu e-posta adresi zaten kullanılmış');
+    // PostgreSQL veritabanına yeni kullanıcı eklenir
+    pool.query('INSERT INTO users (username, surname, email, password) VALUES ($1, $2, $3, $4)', [username, surname, email, hashedPassword], (error, results) => {
+        if (error) {
+            console.error(error);
+            if (error.constraint === 'users_email_key') {
+                res.status(400).send('Bu e-posta adresi zaten kullanılmış');
+            } else {
+                res.status(500).send('Bir hata oluştu, lütfen tekrar deneyin');
+            }
+        } else {
+            res.redirect('/signup-successful');
         }
-
-        // Yeni kullanıcı users.txt dosyasına eklenir
-        users.push(newUser);
-        fs.writeFile('users.txt', JSON.stringify(users, null, 2), 'utf8', (err) => {
-            if (err) throw err;
-            res.redirect('/signup-successful'); // Başarılı kayıt olma sayfasına yönlendirilir
-        });
     });
 });
 
 // Giriş sayfası
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(frontendDirectory, 'login.html')); // Giriş sayfası dosyası gönderilir
+    res.render('login');
 });
 
 // Giriş işlemi
 app.post('/login', (req, res) => {
     const { email, password } = req.body; // Gelen istekten e-posta ve şifre alınır
-    const hashedPassword = crypto.SHA256(password).toString(); // Şifre hashlenir
-
-    // Kullanıcılar dosyası okunur
-    fs.readFile('users.txt', (err, data) => {
-        if (err) throw err;
-        const users = JSON.parse(data);
-        const user = users.find(u => u.email === email);
-
-        if (!user) {
-            res.send('Bu e-posta adresine sahip bir kullanıcı bulunamadı'); // Kullanıcı bulunamadıysa hata gönderilir
-        } else if (user.password !== hashedPassword) {
-            res.send('Şifre yanlış'); // Şifre yanlışsa hata gönderilir
+    
+    // PostgreSQL veritabanından kullanıcıyı bul
+    pool.query('SELECT * FROM users WHERE email = $1', [email], (error, results) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send('Bir hata oluştu, lütfen tekrar deneyin');
         } else {
-            res.redirect(`/login-successful?username=${user.username}&surname=${user.surname}&email=${user.email}`);
-            // Giriş başarılıysa kullanıcı bilgileriyle başarılı giriş sayfasına yönlendirilir
+            if (results.rows.length === 0) {
+                res.status(404).send('Bu e-posta adresine sahip bir kullanıcı bulunamadı');
+            } else {
+                const user = results.rows[0];
+                const hashedPassword = crypto.SHA256(password).toString();
+                if (user.password !== hashedPassword) {
+                    res.status(401).send('Şifre yanlış');
+                } else {
+                    res.redirect(`/login-successful?username=${user.username}&surname=${user.surname}&email=${user.email}`);
+                }
+            }
         }
     });
 });
 
+
 // Kayıt başarılı sayfası
 app.get('/signup-successful', (req, res) => {
-    res.sendFile(path.join(frontendDirectory, 'signup-successful.html')); // Kayıt başarılı sayfası dosyası gönderilir
+    res.render('signup-successful');
 });
 
 // Giriş başarılı sayfası
 app.get('/login-successful', (req, res) => {
     const username = req.query.username; // URL'den kullanıcı adı parametresi alınır
-    res.sendFile(path.join(frontendDirectory, 'login-successful.html')); // Giriş başarılı sayfası dosyası gönderilir
+    res.render('login-successful');
 });
-
-// app.get('/kitapAra', (req, res) => {
-//     res.sendFile(path.join(frontendDirectory, 'kitapAra.html')); // Giriş başarılı sayfası dosyası gönderilir
-// });
-
-
 
 app.post('/kitapAra', (req, res) => {
     const { bookSearch } = req.body;
