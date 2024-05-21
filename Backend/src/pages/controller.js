a89c2aa525638016ffdac6e4f1bb24017af5680b
Esranur Sevilmiş <92573290+esranursevilmis@users.noreply.github.com>
2024-05-21 17:42:55 +0300

    bookdetails sayfasi kitap bilgileri(isbn haric) apiden cekilerek gosteriliyor + frontend islemleri tamamlandi

--- Backend/src/pages/controller.js
+++ Backend/src/pages/controller.js
@@ -1,92 +1,104 @@
-
-const { getBooksList, getUser} = require("./queries.js");
+const { getBooksList, getUser } = require("./queries.js");
 const pool = require("../../db.js");
 
-
 const getMainPage = (req, res) => {
-    const username = req.session ? req.session.username : "";
-    res.render('index', {username: username, msg:req.msg}); // index.ejs dosyasını render et
+  const username = req.session ? req.session.username : "";
+  res.render("index", { username: username, msg: req.msg }); // index.ejs dosyasını render et
 };
 
 const routeMain = (req, res) => {
-    res.redirect('/'); // / adresine yönlendirme yap
+  res.redirect("/"); // / adresine yönlendirme yap
 };
 
 const getBooksPage = (req, res) => {
-    res.render('kitaplik', {username: req.session.username, books: ""}); 
+  res.render("kitaplik", { username: req.session.username, books: "" });
 };
 
 const getCartsPage = async (req, res) => {
-        let value;
-        if(req.params.id){
-            console.log(req.params.id);
-            value = req.params.id;
-        }
-        else{
-            value = 1;
-        }
-        const results = await pool.query(getBooksList, [req.session.userId,value]);
-        const books = results.rows;
-        console.log(books, "Merhabalar");
-        res.render('cart',{value:value ,books:books, username: req.session.username});
+  let value;
+  if (req.params.id) {
+    console.log(req.params.id);
+    value = req.params.id;
+  } else {
+    value = 1;
+  }
+  const results = await pool.query(getBooksList, [req.session.userId, value]);
+  const books = results.rows;
+  console.log(books, "Merhabalar");
+  res.render("cart", {
+    value: value,
+    books: books,
+    username: req.session.username,
+  });
 };
 
 const getPromptPage = (req, res) => {
-    res.render('prompt2', {username: req.session.username}); 
+  res.render("prompt2", { username: req.session.username });
 };
 
 const getContactPage = (req, res) => {
-    res.render('contact', {username: req.session.username}); 
+  res.render("contact", { username: req.session.username });
 };
 
 const getcheckOutPage = (req, res) => {
-    res.render('chackout', {username: req.session.username}); 
+  res.render("chackout", { username: req.session.username });
 };
 
 const getTestPage = (req, res) => {
-    res.render('testimonial', {username: req.session.username}); 
+  res.render("testimonial", { username: req.session.username });
 };
 
 const getSignupPage = (req, res) => {
-    res.render('signup');
+  res.render("signup");
 };
 
 // Giriş sayfası
 const getLoginPage = (req, res) => {
-    res.render('login');
+  res.render("login");
 };
 
 // Kayıt başarılı sayfası
 const signSuccess = (req, res) => {
-    res.render('signup-successful');
+  res.render("signup-successful");
 };
 
-const getBookDetails = (req,res)=>{
-
-    const bookTitle = req.bookTitle;
-    const imageUrl = req.imgURL;
-    return res.render('book-details',{title:bookTitle,author:req.author,publisher:req.publisher, description:req.bookDesc, imgURL : imageUrl, username:req.session.username});
-}
+const getBookDetails = (req, res) => {
+  const bookTitle = req.bookTitle;
+  const imageUrl = req.imgURL;
+ 
+  return res.render("book-details", {
+    title: bookTitle,
+    author: req.author,
+    publisher: req.publisher,
+    description: req.bookDesc,
+    imgURL: imageUrl,
+    username: req.session.username,
+    categories: req.categories,
+    publishedDate: req.publishedDate,
+    pageCount: req.pageCount,
+    language: req.language
+  });
+};
 
 // logoutSuccess olarak değiştirilecek.
 const loginSucces = (req, res, next) => {
-    req.session.destroy();
-    req.msg = "Cikis basarili";
-    next();
+  req.session.destroy();
+  req.msg = "Cikis basarili";
+  next();
 };
 
 module.exports = {
-    getMainPage,
-    getBooksPage,
-    getCartsPage,
-    getContactPage,
-    getcheckOutPage,
-    getPromptPage,
-    getTestPage,
-    routeMain,
-    loginSucces,
-    signSuccess,
-    getSignupPage,
-    getLoginPage,
-    getBookDetails
+  getMainPage,
+  getBooksPage,
+  getCartsPage,
+  getContactPage,
+  getcheckOutPage,
+  getPromptPage,
+  getTestPage,
+  routeMain,
+  loginSucces,
+  signSuccess,
+  getSignupPage,
+  getLoginPage,
+  getBookDetails,
 };
